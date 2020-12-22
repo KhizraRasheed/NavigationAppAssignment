59496ce7d87a48d93adb5c22b6e78f1f0a0d45c7
KhizraRasheed <khizrasheed837@gmail.com>
2020-12-23 01:10:09 +0500

    Assignment completed

--- app/src/main/java/com/example/navigationapp/MainActivity.java
+++ app/src/main/java/com/example/navigationapp/MainActivity.java
@@ -1,14 +1,82 @@
 package com.example.navigationapp;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+public class MainActivity<NavigationView> extends AppCompatActivity {
+
+    NavigationView navigationView;
+
+    DrawerLayout drawerLayout;
+    Toolbar toolbar;
+    ActionBarDrawerToggle toggle;
+
+    @Override
+    public void onBackPressed(){
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+            Toast.makeText(getApplicationContext(),"Start",Toast.LENGTH_LONG).show();
+
+        }
+        else
+        {
+            Toast.makeText(getApplicationContext(),"End",Toast.LENGTH_LONG).show();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        toggle=new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open,R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.nav_quiz:
+                        Toast.makeText(getApplicationContext(), "Retur is Clicked", Toast.LENGTH_LONG).show();
+                        Intent intent = new Intent(MainActivity.this, Ques.class);
+                        startActivity(intent);
+                        //drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+
+                    case R.id.nav_calc:
+                        Toast.makeText(getApplicationContext(), "Retur is Clicked", Toast.LENGTH_LONG).show();
+                        Intent intent1 = new Intent(MainActivity.this, CalcActivity.class);
+                        startActivity(intent1);
+                        //drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+                }
+                return  true;
+            }
+        });
     }
 }
